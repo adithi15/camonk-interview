6245434d9b409719092db023617cb9eea592be58
adithi15 <adithiankam15@gmail.com>
2026-01-18 18:57:27 +0530

    Fix vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/camonk-interview/', // Added the missing comma here
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
